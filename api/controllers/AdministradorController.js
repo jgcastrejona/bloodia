32dc97a0b002d9c6b35b973ece99fd7f77b2e67b
jgcastrejona <jgcastrejona@gmail.com>
2015-09-13 17:54:42 -0500

    API añadidas

---
+++ api/controllers/AdministradorController.js
@@ -0,0 +1,11 @@
+/**
+ * AdministradorController
+ *
+ * @description :: Server-side logic for managing administradors
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/controllers/DonadorController.js
@@ -0,0 +1,11 @@
+/**
+ * DonadorController
+ *
+ * @description :: Server-side logic for managing donadors
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
